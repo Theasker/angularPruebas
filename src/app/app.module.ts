4b580606638d74d2d04aece4ae761bcd8c03efa0
Mauricio Segura <theasker@gmail.com>
2018-05-28 13:37:55 +0200

    componente personalizado para imput

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { LOCALE_ID } from '@angular/core';
-import localeEs from '@angular/common/locales/es';
-import { registerLocaleData } from '@angular/common';
-registerLocaleData(localeEs);
+// import { LOCALE_ID } from '@angular/core';
+// import localeEs from '@angular/common/locales/es';
+// import { registerLocaleData } from '@angular/common';
+// registerLocaleData(localeEs);
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 // Componentes
 import { AppComponent } from './app.component';
-import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 
 // Filtros propios
 import { ThousandsPipe } from './pipes/thousands.pipe';
 
 // Directivas
 import { MyCurrencyFormatterDirective } from './directivas/my-currency-formatter.directive';
+import { InputTextNumberComponent } from './input-text-number/input-text-number.component';
+import { RatingInputComponent } from './rating-input/rating-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReactiveFormsComponent,
     ThousandsPipe,
     MyCurrencyFormatterDirective,
+    InputTextNumberComponent,
+    RatingInputComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule
+    FormsModule
   ],
   exports: [
     MyCurrencyFormatterDirective
   ],
   providers: [
-    { provide: LOCALE_ID, useValue: 'es-ES' },
+    // { provide: LOCALE_ID, useValue: 'es-ES' },
     ThousandsPipe
   ],
   bootstrap: [AppComponent]
---
+++ src/app/input-text-number/input-text-number.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, forwardRef, HostBinding, Input, Output, EventEmitter } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const customValueProvider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => InputTextNumberComponent),
+  multi: true
+};
+
+@Component({
+  selector: 'number-input',
+  template:` 
+  <div class="custom-input">
+    {{ label }} <input type="text" (input)="onChange($event)"  />
+  </div>
+  `,
+  providers: [ customValueProvider  ]
+})
+export class InputTextNumberComponent implements OnInit, ControlValueAccessor {
+  public InputVal: number;
+  _value = '';
+  propagateChange:any = () => {};
+
+  @Input() label: string;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  writeValue(value: any): void {
+    console.log('writeValue value: ', value);
+    if( value ){
+      this._value = value;  
+    }
+  }
+  registerOnChange(fn: any): void {
+    this.propagateChange = fn;
+    console.log('this.propagateChange: ', this.propagateChange);
+  }
+  registerOnTouched(fn: any): void { 
+    console.log('registerOnTouched fn: ', fn);
+  }
+  
+  setDisabledState?(isDisabled: boolean): void {  }
+  onChange(event){
+    console.log('onChange event: ', event);
+    this.propagateChange(event.target.value);
+  }  
+}
